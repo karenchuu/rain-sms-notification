29266b3b18a220303e0f611edd0b4e5ee34f925e
karenchuu <kw83107@gmail.com>
2021-07-24 23:50:59 +0800

    add exception handler

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ weather_params = {
 
 # Check if it Will Rain in the Next 12 Hours
 response = requests.get(OWM_Endpoint, params=weather_params)
-response.raise_for_status()
+# a simple error message, at least
+if response.status_code != 200:
+    print(response.text)
+    print("Make sure the OWM_API_KEY is set properly.")
+    response.raise_for_status()
 weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
 
@@ -37,14 +41,13 @@ will_rain = False
 for hour_data in weather_slice:
     condition_code = hour_data["weather"][0]["id"]
     if int(condition_code) < 600:
-        print("bring an umbrella")
         will_rain = True
+        break
 
 if will_rain:
     try:
         client = Client(twilio_account_sid, twilio_auth_token)
-        message = client.messages \
-            .create(
+        message = client.messages.create(
             body = "今天可能會下雨，記得要帶雨傘! ☔️",
             from_= send_number,
             to = receive_number
